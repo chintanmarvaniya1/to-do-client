2db2c774705235b35a0ca504ba76ca703523334f
chintanmarvaniya1 <chintanmarvaniya985@gmail.com>
2024-03-17 15:49:21 +0530

    Sign up component

--- to-do-client/src/app/app-routing.module.ts
+++ to-do-client/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:LoginComponent,
+    path:'login'
+  },
+  {
+    component:SignupComponent,
+    path:'signup'
+  },
+  {
+    component:HomeComponent,
+    path:'home'
+  },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
